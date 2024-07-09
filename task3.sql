1a23e53787aa5e5e1713d64cc20164ac3795d154
Мосиенко Юлия <152617308+julimosienko@users.noreply.github.com>
2024-07-10 01:42:42 +0300

    Create task3.sql

---
+++ task3.sql
@@ -0,0 +1,5 @@
+--cоздать базы для стейджинга, исторических данных, текущих данных и буферных таблиц
+create database stg;
+create database current;
+create database history;
+create database direct_log;
