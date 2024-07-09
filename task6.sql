f27cf1f9a3b4a9e7d8bb7f98ab0427a09a876958
Мосиенко Юлия <152617308+julimosienko@users.noreply.github.com>
2024-07-10 02:15:08 +0300

    Update task6.sql

--- task6.sql
+++ task6.sql
@@ -4,15 +4,8 @@ create table if not exists current.tareLoad
     tare_id       UInt64,
     is_load       UInt8,
     dt            DateTime,
-    tare_sticker  String,
-    tare_type     LowCardinality(String),
     office_id     UInt32,
-    dst_office_id UInt32 comment 'load>0, unload=0',
-    wh_tare_entry String,
-    dt_reported   DateTime,
-    task_id       LowCardinality(String),
-    licence_plate LowCardinality(String),
-    dt_load       DateTime materialized now()
+    dst_office_id UInt32 comment 'load>0, unload=0'
 )
     engine = ReplacingMergeTree()
     ORDER BY (is_load, tare_id)
@@ -24,26 +17,13 @@ TO current.tareLoad
     tare_id       UInt64,
     is_load       UInt8,
     dt            DateTime,
-    tare_sticker  String,
-    tare_type     LowCardinality(String),
     office_id     UInt32,
-    dst_office_id UInt32 comment 'load>0, unload=0',
-    wh_tare_entry String,
-    dt_reported   DateTime,
-    task_id       LowCardinality(String),
-    licence_plate LowCardinality(String),
-    dt_load       DateTime materialized now()
+    dst_office_id UInt32 comment 'load>0, unload=0'
 )
 AS
 SELECT tare_id
     , is_load
     , dt
-    , tare_sticker
-    , tare_type
     , office_id
     , dst_office_id
-    , wh_tare_entry
-    , dt_reported
-    , task_id
-    , licence_plate
 FROM stg.tareLoad;
